a254d4229a163c778f85d37120b310838e72bdb5
Stuart Long <chayboon@gmail.com>
2026-03-01 23:13:05 +0800

    chore: change icon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -14,19 +14,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/sparkle.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/sparkle.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/sparkle.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/sparkle.png',
   },
 }
 
